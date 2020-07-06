6bef92c21004b8ad48c231170a876903b917d821
chitra <chitraflorid@users.noreply.github.com>
2020-07-06 12:54:42 +0530

    Update buttonsGrid.js

--- js/buttonsGrid.js
+++ js/buttonsGrid.js
@@ -49,37 +49,3 @@
     return color;
    }
 });
-
-const btnIndexList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-class ButtonGrid {
-
-  static get btnBackgroundcolors() { 
-    return ['#2b8ead', '#2f454e', '#bfbfbf', '#6f98a8']; 
-  }
-  
-  static shuffleBtns() {
-      for (let i = btnIndexList.length - 1; i > 0; i--) {
-       	const newPos = Math.floor(Math.random() * (i + 1));
-          [btnIndexList[newPos], btnIndexList[i]]  = [btnIndexList[i], btnIndexList[newPos]];
-     }
-       updateBtnsGrid();        
-  }  
-  
-  sortBtns() {
-  		btnIndexList.sort((a,b) => a - b);
-			updateBtnsGrid();
-  }  
-  
-  
-  getColor() {
-   
-   if (btnBackgroundColors.length === 0) {
-       btnBackgroundColors = ['#2b8ead', '#2f454e', '#bfbfbf', '#6f98a8'];
-   }
-   var randIndex = parseInt(Math.random() * btnBackgroundColors.length);
-   var color = btnBackgroundColors.splice(randIndex, 1);
-   return color;
-  }
-  
-}
