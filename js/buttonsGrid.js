227a7b476b267aaea7deff4a9976b4400f779a15
chitra <chitraflorid@users.noreply.github.com>
2020-07-06 13:05:01 +0530

    Update buttonsGrid.js

--- js/buttonsGrid.js
+++ js/buttonsGrid.js
@@ -1,51 +1,67 @@
-  document.addEventListener('DOMContentLoaded', () => {
+
+
+class ButtonGrid {
+  constructor() {
+    this.btnIndexList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    this.btnBackgroundColors = ['#2b8ead', '#2f454e', '#bfbfbf', '#6f98a8'];
+  }
+  
+  shuffleBtns() {
+    const { btnIndexList } = this;
+    
+    for (let i = btnIndexList.length - 1; i > 0; i--) {
+     	const newPos = Math.floor(Math.random() * (i + 1));
+        [btnIndexList[newPos], btnIndexList[i]]  = [btnIndexList[i], btnIndexList[newPos]];
+   }
+     this._updateBtnsGrid();  
+  }
+  
+  _updateBtnsGrid() {
+    const { btnIndexList } = this;
+    
+		btnIndexList.forEach((val, index) => {
+    		const btn = document.querySelector(`#btn-${index + 1}`);
+    		btn.innerHTML = val;
+        btn.style.backgroundColor = this._getColor();
+        btn.className = 'box-transition';
+    });
+  }
+  
+  sortBtns() {
+      const { btnIndexList } = this;
+  		btnIndexList.sort((a,b) => a - b);
+			this._updateBtnsGrid();
+  }   
+  
+  _getColor() {
+    const bgColorLen = this.btnBackgroundColors.length;
+    
+    if (bgColorLen === 0) {
+       this.btnBackgroundColors = ['#2b8ead', '#2f454e', '#bfbfbf', '#6f98a8'];
+    }
+    
+    const randIndex = parseInt(Math.random() * bgColorLen);
+    const color = this.btnBackgroundColors.splice(randIndex, 1);
+    
+    return color;
+ }
+}    
+  
+document.addEventListener('DOMContentLoaded', () => {
      document.querySelector('#action-btns').addEventListener('click', handleActionBtnClick);
-     const btnIndexList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-     let btnBackgroundColors = ['#2b8ead', '#2f454e', '#bfbfbf', '#6f98a8'];
+     const btnGrid = new ButtonGrid();
       
     function handleActionBtnClick(event) {
     		const btn = event.target.getAttribute('id');
     		
         switch(btn) {
         		case 'shuffle':
-            	shuffleBtns();
+            	btnGrid.shuffleBtns();
             	break;
             
             case 'sort':
-            	sortBtns();
+            	btnGrid.sortBtns();
             	break;
         }
     }
-
-             
-   function shuffleBtns() {
-       for (let i = btnIndexList.length - 1; i > 0; i--) {
-        	const newPos = Math.floor(Math.random() * (i + 1));
-           [btnIndexList[newPos], btnIndexList[i]]  = [btnIndexList[i], btnIndexList[newPos]];
-      }
-        updateBtnsGrid();        
-   }  
-   
-   function sortBtns() {
-   		btnIndexList.sort((a,b) => a - b);
- 			updateBtnsGrid();
-   }   
-   
-   function updateBtnsGrid() {
-   			btnIndexList.forEach((val, index) => {
-        		const btn = document.querySelector(`#btn-${index + 1}`);
-        		btn.innerHTML = val;
-            btn.style.backgroundColor = getColor();
-            btn.className = 'box-transition';
-        });
-   }
-   
-   function getColor() {
-    if (btnBackgroundColors.length === 0) {
-        btnBackgroundColors = ['#2b8ead', '#2f454e', '#bfbfbf', '#6f98a8'];
-    }
-    var randIndex = parseInt(Math.random() * btnBackgroundColors.length);
-    var color = btnBackgroundColors.splice(randIndex, 1);
-    return color;
-   }
 });
